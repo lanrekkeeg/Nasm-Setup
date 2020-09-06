fbe331e1b811fd5b193d65f5616a69b3b9e0d6a9
lanrrekkeeg <p156058@nu.edu.pk>
2020-09-06 11:23:47 +0500

    adding nasm debugger files

---
+++ setup.sh
@@ -0,0 +1,15 @@
+
+# updating the current system
+sudo apt-get update
+# installing dosbox
+sudo apt-get install dosbox
+
+# creating repository
+sudo mkdir -p ~/nasm-lab
+
+# moving files to nasm-lab
+
+sudo cp -r * ~/nasm-lab
+
+# adding automatic mounting in dosbox configuration file
+
