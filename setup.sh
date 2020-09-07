98ea8477e73ea9d770f68c037dfecd202041e77f
lanrrekkeeg <p156058@nu.edu.pk>
2020-09-07 07:07:22 +0500

    killall command added

--- setup.sh
+++ setup.sh
@@ -17,3 +17,7 @@ sleep 4s
 killall dosbox
 dir="$HOME/nasm-lab/"
 sudo sed "/autoexec/ a mount c $dir\nC:" ~/.dosbox/dosbox-0.74.conf |sudo tee -a ~/.dosbox/dosbox-0.74.conf
+
+echo "################SETUPT FINISHED####################"
+echo "PLEASE TAKE A NOTE OF WORKING DIRECTROY WHERE YOU WILL BE DOING YOUR LABS"
+echo "working Directory:$dir"
